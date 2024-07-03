3cefcd6833c320af98b9a8ed5114a5cf94360af5
Naveenkumar4510 <naveenkuma1045@gmail.com>
2024-07-03 19:04:26 +0530

    started iew bus details

---
+++ src/Component/pages/Bus/ViewBus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { bus_detailAction } from '../../../redux/action/bus'
+const ViewBus = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ViewBus
